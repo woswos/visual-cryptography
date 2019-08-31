a487a21c52959d5b11a9c610e67f20d21c2cacb5
Barkin Simsek <barkin.simsek@hotmail.com>
2019-08-31 15:18:15 +0400

    added new functions

--- python/test.py
+++ python/test.py
@@ -33,6 +33,21 @@ def saveImage(img, fileName):
     PIL.Image.fromarray(img).save(fileName)
     print("Saved output file: ", fileName)
 
+def drawImage(dist):
+    total_p = 0.0
+    for prob, _ in dist:
+        assert prob > -0.0001 and prob < 1.0001
+        total_p += prob
+    assert total_p > -0.0001 and total_p < 1.0001
+
+    r = randfloat()
+
+    for prob, outcome in dist:
+        if r < prob:
+            return outcome
+        r -= prob
+
+    return dist[-1][1]
 
 def noise1in2out(img):
     shape = img.shape
@@ -52,6 +67,45 @@ def noise1in2out(img):
             out2[i, j] = o2 * 255
 
     return (out1, out2)
+
+
+def noise3in3out(img1, img2, img3):
+    shape = img1.shape
+    if shape != img2.shape or shape != img3.shape:
+        raise TypeError('all three images must have the same shape')
+
+    out1 = np.zeros(shape, dtype=np.uint8)
+    out2 = np.zeros(shape, dtype=np.uint8)
+    out3 = np.zeros(shape, dtype=np.uint8)
+
+    # Scale the images into range
+    contrast = 1/6
+    img1 = img1 * contrast
+    img2 = img2 * contrast
+    img3 = img3 * contrast
+
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            temp1 = img1[i,j]
+            temp2 = img2[i,j]
+            temp3 = img3[i,j]
+
+            o1, o2, o3 = drawImage([
+                (0, (1,1,1)),
+                (temp1 + temp2 + temp3, (0,0,0)),
+                (temp3, (0,1,1)),
+                (temp2, (1,0,1)),
+                (temp1, (1,1,0)),
+                (1/3 - temp1 - temp2, (1,0,0)),
+                (1/3 - temp1 - temp3, (0,1,0)),
+                (1/3 - temp2 - temp3, (0,0,1))
+            ])
+
+            out1[i,j] = o1 * 255
+            out2[i,j] = o2 * 255
+            out3[i,j] = o3 * 255
+
+    return (out1, out2, out3)
 
 
 def steganography3in2out(clear1, clear2, secret):
@@ -92,16 +146,6 @@ def steganography3in2out(clear1, clear2, secret):
             out1[i, j] = o1 * 255
             out2[i, j] = o2 * 255
 
-            # Propagate the errors using the Floyd-Steinberg kernel
-            # def prop(mat, di, dj, err):
-            #    if i+di < mat.shape[0] and 0 <= j+dj < mat.shape[1]:
-            #        mat[i+di][j+dj] += 0.25*err
-
-            #prop(clear1, 0,  1, (a-o1) * (7/16))
-            #prop(clear1, 1, -1, (a-o1) * (3/16))
-            #prop(clear1, 1,  0, (a-o1) * (5/16))
-            #prop(clear1, 1,  1, (a-o1) * (1/16))
-
     return (out1, out2)
 
 
@@ -114,15 +158,26 @@ if __name__ == '__main__':
 
     inputFilesParsed = json.loads(inputFiles)
 
-    if(selectedAlgorithm == "noise1in2out"):
-	    image = loadImage(inputFilesParsed["0"])
+    if ((selectedAlgorithm == "noise1in2out") or (selectedAlgorithm == "0")):
+        image = loadImage(inputFilesParsed["0"])
         outputImages = noise1in2out(image)
-        saveImage(outputImages[0], "1.png")
-        saveImage(outputImages[1], "2.png")
+        saveImage(outputImages[0], "result/1.png")
+        saveImage(outputImages[1], "result/2.png")
 
-	elif(selectedAlgorithm == "steganography3in2out"):
-        outputImages = steganography3in2out(image)
-        saveImage(outputImages[0], "1.png")
-        saveImage(outputImages[1], "2.png")
+    elif((selectedAlgorithm == "steganography3in2out") or (selectedAlgorithm == "1")):
+        clear1 = loadImage(inputFilesParsed["0"])
+        clear2 = loadImage(inputFilesParsed["1"])
+        secret = loadImage(inputFilesParsed["2"])
+        outputImages = steganography3in2out(clear1, clear2, secret)
+        saveImage(outputImages[0], "result/1.png")
+        saveImage(outputImages[1], "result/2.png")
 
+    elif((selectedAlgorithm == "noise3in3out") or (selectedAlgorithm == "2")):
+        image1 = loadImage(inputFilesParsed["0"])
+        image2 = loadImage(inputFilesParsed["1"])
+        image3 = loadImage(inputFilesParsed["2"])
+        outputImages = noise3in3out(image1, image2, image3)
+        saveImage(outputImages[0], "result/1.png")
+        saveImage(outputImages[1], "result/2.png")
+        saveImage(outputImages[2], "result/3.png")
 # print "default_5.png;default_6.png;"
